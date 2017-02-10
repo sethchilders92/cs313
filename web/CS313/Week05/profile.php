5cd38d2d7b8baf30c88fd8943f21dfdce11aec64
Seth Childers <chi11008@byui.edu>
2017-02-09 19:07:29 -0700

    Trying php variables in the SQL statement

--- web/CS313/Week05/profile.php
+++ web/CS313/Week05/profile.php
@@ -4,29 +4,17 @@ require "dbconnect.php";
 $db = get_db();
 ?>
  <?php
-$statement = $db->prepare("SELECT firstname, lastname, description, id FROM profile WHERE firstname='Seth'");
-$statement2 = $db->prepare("SELECT username, password, id FROM person WHERE username='sethchilders92'");
+if($_POST['Jon']) {
+    $id = 2;
+} else
+    $id = 1;
+
+$statement = $db->prepare("SELECT firstname, lastname, description, id FROM profile WHERE id=$id");
+$statement2 = $db->prepare("SELECT username, password, id FROM person WHERE id=$id");
 $statement->execute();
 // Go through each result
 $row = $statement->fetch(PDO::FETCH_ASSOC);
-$row2 = $statement2->fetch(PDO::FETCH_ASSOC);
-//while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-//{
-//	echo '<strong>' . $row['firstname'] . " " . $row['lastname'];
-//    echo ' ' . ' </strong> - ' . $row['description'];
-//    echo ':' . $row['id'];
-//    echo '<br/>';
-//}
-//
-//$statement2->execute();
-//// Go through each result
-//while ($row = $statement2->fetch(PDO::FETCH_ASSOC))
-//{
-//	echo '<strong>' . "Username and Password: " . $row['username'] . " - " . $row['password'];
-//    echo '</strong>';
-//    echo ' ' . $row['id'];
-//    echo '<br/>';
-//}   
+$row2 = $statement2->fetch(PDO::FETCH_ASSOC);  
 
 ?>      
 
@@ -35,24 +23,29 @@ $row2 = $statement2->fetch(PDO::FETCH_ASSOC);
     <head>
         <title>
             <?php 
-                echo $row['firstname'] . " " . $row['lastname'];
+                echo "General Profile | "$row['firstname'] . " " . $row['lastname'];
             ?>
             </title>
+        <link type="text/css" rel="stylesheet" href="main.css"/>
     </head>
     <body>
        
         
         <div class="container">
             <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRLYse2XyJuK0MfQWeNpTkyliPUkcLOJMMon36q-zrtEAW4T2oq" width="100%">
-        <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSM7ht8eWmGu6BSM6J43Kn4s2qGzezX40KNmhS60-CMXsUMZ0MzLw" width="30%"/>
-            <h2><?php 
+        <img class="profilePic" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSM7ht8eWmGu6BSM6J43Kn4s2qGzezX40KNmhS60-CMXsUMZ0MzLw" width="30%"/>
+            <h1 class="name"><?php 
                     echo $row['firstname'] . " " . $row['lastname'];
-                ?></h2>
+                ?></h1>
         <div class="description">
             <h4>Description</h4>
             <p><?php 
                     echo $row['description'];
                 ?></p>
+            <form method="post">
+            <input name="Jon" method="post" type="radio"/>Jonathan's Profile
+            <input name="Seth" method="post" type="radio"/>Seth's Profile
+            </form>
             </div>
         </div>
     </body>

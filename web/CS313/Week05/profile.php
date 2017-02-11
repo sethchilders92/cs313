24cf0fc589cd4411b485edc3a5ca598fec2dd2e8
Seth Childers <chi11008@byui.edu>
2017-02-10 18:39:24 -0700

    e

--- web/CS313/Week05/profile.php
+++ web/CS313/Week05/profile.php
@@ -3,28 +3,28 @@
 require "dbconnect.php";
 $db = get_db();
 
-if($_POST['username'] == 'sethchilders92') {
-    $id = 1;
-} else
-    $id = 2;
+//if($_POST['username'] == 'sethchilders92') {
+//    $id = 1;
+//} else
+//    $id = 2;
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$userInfo = "SELECT * FROM person WHERE username=$username";
+//$userInfo = "SELECT * FROM person WHERE username=$username";
 
-//$statement = $db->prepare("SELECT * FROM person WHERE username=$username");
-//$statement->execute();
-// Go through each result
-//$row = $statement->fetch(PDO::FETCH_ASSOC);  
+$statement = $db->prepare("SELECT * FROM person WHERE username=$username");
+$statement->execute();
+ //Go through each result
+$row = $statement->fetch(PDO::FETCH_ASSOC);  
 
-//$id = $row['id'];
-
-$sql = "SELECT * FROM profile WHERE id=$id";
-$statement2 = $db->prepare($sql);
-$statement2->execute();
-$row2 = $statement2->fetch(PDO::FETCH_ASSOC);
-
+$id = $row['id'];
+//
+//$sql = "SELECT * FROM profile WHERE id=$id";
+//$statement2 = $db->prepare($sql);
+//$statement2->execute();
+//$row2 = $statement2->fetch(PDO::FETCH_ASSOC);
+echo "Here's the id: " . $id;
 
 ?>      
 
@@ -41,19 +41,22 @@ $row2 = $statement2->fetch(PDO::FETCH_ASSOC);
     <body> 
        
         
+<!--
         <div class="container">
             <img class="background" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRLYse2XyJuK0MfQWeNpTkyliPUkcLOJMMon36q-zrtEAW4T2oq" width="100%">
         <img class="profilePic" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSM7ht8eWmGu6BSM6J43Kn4s2qGzezX40KNmhS60-CMXsUMZ0MzLw"/>
             <h1 class="name"><?php 
-                    echo $row2['firstname'] . " " . $row2['lastname'];
+                    //echo $row2['firstname'] . " " . $row2['lastname'];
                 ?></h1>
         <div class="description">
             <h4>Description</h4>
             <p><?php 
-                    echo $row2['description'];
+               //     echo 
+                 //   echo $row2['description'];
                 ?></p>
             </div>
         </div>
+-->
     </body>
 </html>
 

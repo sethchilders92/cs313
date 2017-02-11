dd5bdf8128f5e0f0a83a8f33e97978ecf28b6d7c
Seth Childers <chi11008@byui.edu>
2017-02-10 18:33:17 -0700

    e

--- web/CS313/Week05/profile.php
+++ web/CS313/Week05/profile.php
@@ -3,17 +3,17 @@
 require "dbconnect.php";
 $db = get_db();
 
-if($_POST['username'] == 'sethchilders92') {
-    $id = 1;
-} else
-    $id = 2;
+//if($_POST['username'] == 'sethchilders92') {
+//    $id = 1;
+//} else
+//    $id = 2;
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 $userInfo = "SELECT * FROM person WHERE username=$username";
 
-$statement = $db->prepare('SELECT * FROM person WHERE username=$username');
+$statement = $db->prepare("SELECT * FROM person WHERE username=$username");
 $statement->execute();
 // Go through each result
 //$row = $statement->fetch(PDO::FETCH_ASSOC);  
